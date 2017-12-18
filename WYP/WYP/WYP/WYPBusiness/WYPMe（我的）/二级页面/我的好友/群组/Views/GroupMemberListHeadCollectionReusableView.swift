3ad59aea7a11ec9d5569de7014081cb030f2b0d4
曾雪峰 <zengxuefeng@163.com>
2017-12-18 09:58:01 +0800

    群组公告

---
+++ WYP/WYP/WYP/WYPBusiness/WYPMe（我的）/二级页面/我的好友/群组/Views/GroupMemberListHeadCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  GroupMemberListHeadCollectionReusableView.swift
+//  WYP
+//
+//  Created by 曾雪峰 on 2017/12/15.
+//  Copyright © 2017年 NGeLB. All rights reserved.
+//
+
+import UIKit
+
+class GroupMemberListHeadCollectionReusableView: UICollectionReusableView {
+        
+}
---
+++ WYP/WYP/WYP/WYPBusiness/WYPMe（我的）/二级页面/我的好友/群组/Views/GroupNoteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GroupNoteTableViewCell.swift
+//  WYP
+//
+//  Created by 曾雪峰 on 2017/12/17.
+//  Copyright © 2017年 NGeLB. All rights reserved.
+//
+
+import UIKit
+
+class GroupNoteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
